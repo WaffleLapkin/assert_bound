18b8b91c32eda527f6533afcc2e8647cc9649254
Waffle <waffle.lapkin@gmail.com>
2019-11-30 00:30:06 +0300

    Completely rewrite crate. - Paths to trait and generics are now fully supported (e.g. you can now write `assert_bound!(() => std::fmtDebug + Ord + PartialEq<()>)`) - Rename `as_trait` => `as_opaque` - Add `#[inline(always)]` attrs on inner functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,95 +1,185 @@
-//! When working with iterators/futures or other hign-generic types sometimes 
-//! it's useful to assert that type of some expression implements some traits or even cast smt to `impl Trait`. 
+//! When working with iterators/futures or other hign-generic types sometimes it's useful to assert
+//! that type of some expression implements some traits or even cast smt to `impl Trait`.
 //! 
-//! This crate provides macroses for both goals — [`assert_bound`] and [`as_trait`].
-//! 
+//! This crate provides macros for both goals — [`assert_bound`] and [`as_opaque`].
+//!
 //! [`assert_bound`]: ./macro.assert_bound.html
-//! [`as_trait`]: ./macro.as_trait.html
-
+//! [`as_opaque`]: ./macro.as_opaque.html
 
 /// Assert that expression implements trait(s) at compile-time.
-/// 
+///
 /// ## Examples
 /// ```
 /// use assert_bound::assert_bound;
 /// use std::fmt::Debug;
-/// 
-/// assert_bound!(&(); Debug + Ord + PartialEq<()>)
+///
+/// assert_bound!(() => Debug + Ord + PartialEq<()>);
 /// ```
-/// 
+///
 /// ```
 /// # use assert_bound::assert_bound;
 /// trait T {}
 /// impl T for () {}
-/// 
-/// assert_bound!(&(); T);
+///
+/// assert_bound!(() => T);
 /// ```
-/// 
+/// **Note**: expression is **not** executed:
+/// ```
+/// # use assert_bound::assert_bound;
+/// let mut var = 0;
+/// assert_bound!({ var = 1; } => Eq);
+/// assert_eq!(var, 0);
+/// ```
+/// However you can execute it by calling result of the macro:
+/// ```
+/// # use assert_bound::assert_bound;
+/// let mut var = 0;
+/// assert_bound!({ var = 1; } => Eq)();
+/// assert_eq!(var, 1);
+/// ```
 /// ```compile_fail
 /// # use assert_bound::assert_bound;
 /// // f32/f64 doesn't implement `Eq`,
 /// // so rustc will fail to compile that.
-/// assert_bound!(&0.1; Eq);
+/// assert_bound!(0.1; Eq);
 /// ```
-/// **NOTE**: under the hood this macro uses function that accepts `&T` where `T: *bounds*` so you **need** to pass reference and those examples will fail to compile:
-/// ```compile_fail
-/// # use assert_bound::assert_bound;
-/// assert_bound!((); Eq);
+/// ## Expand
+/// Following code:
 /// ```
-/// 
-/// ```compile_fail
 /// # use assert_bound::assert_bound;
-/// trait T {}
-/// impl T for () {}
-/// 
-/// assert_bound!(&&(); T);
+/// assert_bound!({ println!("Hewwo?") } => Ord + PartialEq<()>);
+/// ```
+/// expands to something like this:
+/// ```
+/// || {
+///     fn assert_bound<T>(_: &T)
+///     where
+///         T: Ord,
+///         T: PartialEq<()>,
+///     {}
+///
+///     let expr = { println!("Hewwo?") };
+///     assert_bound(&expr);
+///     expr
+/// };
 /// ```
 #[macro_export]
 macro_rules! assert_bound {
-    ($i:expr; $bound:tt $(<$param:tt $(,$p:tt)*>)? $(+ $b:tt $(<$param_:tt $(,$p_:tt)*>)?)*) => {
-        // new scope, so there won't be callision with assert_bound name
-        {
-            fn assert_bound<__T: $bound $(<$param $($p)*>)? $(+ $b $(<$param_ $($p_)*>)?)*>(_: &__T) {}
+    (
+        $e:expr =>
+        // One trait bound, e.g. `std::fmt::Debug`, `PartialEq<()>`
+        $head:ident $( :: $tail:ident )* $( < $param:ty $(, $p:ty)* > )?
+        // Zero or more trait bounds splited by `+`
+        $(+ $head2:ident $( :: $tail2:ident )* $( < $param2:ty $(, $p2:ty)* > )?)*
+    ) => {
+        // Lambda is needed for discarding $e (lambda is returned from macro so
+        // it's possible to call it in order to not discard)
+        || {
+            /// Assert that `T` implements traits these were given to macro
+            #[inline(always)]
+            fn assert_bound<__EXPR_TYPE>(_: &__EXPR_TYPE)
+            where
+                __EXPR_TYPE: $head $( :: $tail )* $( < $param $(, $p)* > )?,
+                $( __EXPR_TYPE: $head2 $( :: $tail2 )* $( < $param2 $(, $p2)* > )? , )*
+            {}
 
-            assert_bound($i);
+            let expr = $e;
+            // Assert that $e implement traits
+            assert_bound(&$e);
+            // Return $e from lambda
+            expr
         }
-    }
+    };
 }
 
-/// Cast type to anonymous type (`impl Trait`) 
-/// 
+/// Cast type to opaque type that implements trait(s) (`impl Trait + Trait2`)
+///
 /// ```
-/// # use assert_bound::as_trait;
-/// use std::fmt::Debug;
-/// 
-/// println!("{:?}", as_trait!((); Debug)); // `as_trait!` return `impl Debug`
+/// # use assert_bound::as_opaque;
+///
+/// println!("{:?}", as_opaque!(() => std::fmt::Debug)); // `as_opaque!` return `impl Debug`
 /// ```
-/// 
+///
 /// ```
-/// # use assert_bound::as_trait;
-/// let t = as_trait!((); PartialEq<()> + PartialOrd<()>);
-/// 
+/// # use assert_bound::as_opaque;
+/// let t = as_opaque!(() => PartialEq<()> + PartialOrd<()>);
+///
 /// assert!(t == ());
 /// assert_eq!(t.partial_cmp(&()), Some(std::cmp::Ordering::Equal));
 /// ```
-/// 
+///
 /// ```compile_fail
-/// # use assert_bound::{as_trait, assert_bound};
+/// # use assert_bound::{as_opaque, assert_bound};
 /// assert_bound!(&(), Eq); // OK
-/// assert_bound!(&as_trait!((); Ord), Ord); // OK
-/// assert_bound!(&as_trait!((); Ord), Eq); // Error
+/// assert_bound!(&as_opaque!((); Ord), Ord); // OK
+/// assert_bound!(&as_opaque!((); Ord), Eq); // Error
+/// ```
+///
+/// ## Expand
+/// Following code:
+/// ```
+/// # use assert_bound::as_opaque;
+/// as_opaque!(() => PartialEq<()> + PartialOrd<()>);
+/// ```
+/// expands to something like this:
+/// ```
+/// fn as_opaque<T>(expr: T) -> impl PartialEq<()> + PartialOrd<()> + 'static
+/// where
+///     T: PartialEq<()>,
+///     T: PartialOrd<()>,
+///     T: 'static,
+/// {
+///     expr
+/// }
+///
+/// let expr = ();
+/// as_opaque(expr);
 /// ```
 #[macro_export]
-macro_rules! as_trait {
-    ($i:expr; $bound:tt $(<$param:tt $(,$p:tt)*>)? $(+ $b:tt $(<$param_:tt $(,$p_:tt)*>)?)*) => {
-        // new scope, so there won't be callision with as_trait ident
-        {
-            fn as_trait<__T: $bound $(<$param $($p)*>)? $(+ $b $(<$param_ $($p_)*>)?)*>(x: __T) -> impl $bound $(<$param $($p)*>)? $(+ $b $(<$param_ $($p_)*>)?)* {
-                x
+macro_rules! as_opaque {
+    (
+        $e:expr =>
+        // One trait bound, e.g. `std::fmt::Debug`, `PartialEq<()>`
+        $head:ident $( :: $tail:ident )* $( < $param:ty $(, $p:ty)* > )?
+        // Zero or more trait bounds splited by `+`
+        $(+ $head2:ident $( :: $tail2:ident )* $( < $param2:ty $(, $p2:ty)* > )?)*
+        // lifetime
+        ; $lifetime:tt
+    ) => {
+            {
+                /// Cast type to anonymous type that implements trait(s) these were given to macro
+                #[inline(always)]
+                fn as_opaque<'lifetime, __EXPR_TYPE>(expr: __EXPR_TYPE)
+                    -> impl $head $( :: $tail )* $( < $param $(, $p)* > )?
+                    $(+ $head2 $( :: $tail2 )* $( < $param2 $(, $p2)* > )? )*
+                    + 'lifetime
+                where
+                    __EXPR_TYPE: $head $( :: $tail )* $( < $param $(, $p)* > )?,
+                    $( __EXPR_TYPE: $head2 $( :: $tail2 )* $( < $param2 $(, $p2)* > )? , )*
+                    __EXPR_TYPE: 'lifetime,
+                {
+                    expr
+                }
+
+                let expr = $e;
+                let opaque = as_opaque::<$lifetime, _>(expr);
+                opaque
             }
+    };
 
-            as_trait($i)
-        }
-    }
+    // Variant without lifetime (use default 'static)
+    (
+        $e:expr =>
+        // One trait bound, e.g. `std::fmt::Debug`, `PartialEq<()>`
+        $head:ident $( :: $tail:ident )* $( < $param:ty $(, $p:ty)* > )?
+        // Zero or more trait bounds splited by `+`
+        $(+ $head2:ident $( :: $tail2:ident )* $( < $param2:ty $(, $p2:ty)* > )?)*
+    ) => {
+        $crate::as_opaque!(
+            $e =>
+            $head $( :: $tail )* $( < $param $(, $p)* > )?
+            $(+ $head2 $( :: $tail2 )* $( < $param2 $(, $p2)* > )?)*
+            ; 'static
+        )
+    };
 }
-
